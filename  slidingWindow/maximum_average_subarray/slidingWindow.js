dcbd63c79fd088dfe4d8a9a41b3ff13748090c90
Rehema Mwaka <mwakabayah@gmail.com>
2024-05-03 16:50:27 +0300

    Update logic to return maximum average

---  slidingWindow/maximum_average_subarray/slidingWindow.js
+++  slidingWindow/maximum_average_subarray/slidingWindow.js
@@ -1,24 +1,38 @@
 const findAvgOfSubarrays = (arr, k) => {
     //sliding window approach
-let results = 0;
-let windowStart = 0;
-let windowSum = 0;
 
-for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-  windowSum += arr[windowEnd];
+    const results = []
+    let windowSum = 0;
+    let windowStart = 0;
 
-  if (windowEnd >= k - 1) {
-    let avg = windowSum / k;
-    if (avg >= results) {
-      results = avg;
+    for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+        //add the next element
+        windowSum += arr[windowEnd]
+
+        //slide the window foward
+        //we don't need to slide if we have not hit the required window size of k
+        if( windowEnd >= k-1){
+            //we are **Automatically** returning the window average once we have hit the window size
+            //and pushing to the output array
+            let avg = windowSum / k;
+            if (results.length === 0) results.push(avg);
+            if (avg >= results[0]) {
+              results.pop();
+              results.push(avg);
+            }
+            // results.push(avg)
+        
+        //subtract element going out of sliding window
+        windowSum -= arr[windowStart]
+
+        //then sliding the window foward
+        windowStart++
+
+        //adding the element coming in, in the outer/previous loop
+        //and repeating this process untill we ht the end of the array
     }
-
-    windowSum -= arr[windowStart];
-
-    windowStart++;
-  }
 }
-return results;
+return results[0];
 };
 
 const arr = [1, 3, 2, 6, -1, 4, 1, 8, 2];
